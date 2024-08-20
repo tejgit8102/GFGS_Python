eb44834ba3467c59d512fb6a89fcd86e3e46bf6a
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-08-20 21:37:09 +0530

    Add files via upload

---
+++ Program7.py
@@ -0,0 +1,13 @@
+def welcomeAboard(name):
+    print("Welcome", name)
+
+# Call the function with the name 'John'
+welcomeAboard("John")
+
+
+'''The code we type in the editor of GFGS is as below
+
+def welcomeAboard(name):
+    print("Welcome", name)
+    
+'''
