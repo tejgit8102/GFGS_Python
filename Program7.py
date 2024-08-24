966f6a5ed4cafa6b29cd3f94476aee5ec5853fb8
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-08-24 18:32:32 +0530

    Update Program7.py

--- Program7.py
+++ Program7.py
@@ -1,3 +1,27 @@
+'''This module talks about Strings in Python. String in Python is immutable (cannot be edited). You have learnt about separators in Python. Let's start String with first question given below:
+
+Given name of a person, the task is to welcome the person by printing the name with "Welcome". If name is "John", you should print "Welcome John".
+
+Example 1:
+
+Input:
+name = John
+Output:
+Welcome John
+Example 2:
+
+Input:
+name = Python
+Output:
+Welcome Python
+Your Task:
+This is a function problem. You need to complete the function welcomeAboard() and print the required string.
+
+Constraints:
+1 <= |name| <= 100'''
+
+#CODE:
+
 def welcomeAboard(name):
     print("Welcome", name)
 
